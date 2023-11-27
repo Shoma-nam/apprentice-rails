1e7a010031cd67cf1620f824017cde20157aa52e
shome <yubemto12@gmail.com>
2023-11-27 23:59:04 +0900

    Add style.css

---
+++ Documents/apprentice-team-product/welcome.js
@@ -0,0 +1,9 @@
+document.getElementById('add-link').addEventListener('click', function() {
+    // リンク追加ボタンのクリック時の動作
+    alert('リンク追加ボタンがクリックされました！');
+});
+
+document.getElementById('saved-links').addEventListener('click', function() {
+    // 保存済みリンクボタンのクリック時の動作
+    alert('保存済みリンクボタンがクリックされました！');
+});
